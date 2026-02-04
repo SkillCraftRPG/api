87fdc0f1a8761321a1d85953d71dbb9ae00c0357
Francis Pion <francispion@hotmail.com>
2026-02-03 21:51:19 -0500

    infra

---
+++ src/SkillCraft.Api.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Logitar.EventSourcing.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SkillCraft.Api.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddSkillCraftApiInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddSingleton<IEventSerializer, EventSerializer>()
+      .AddScoped<IEventBus, EventBus>();
+  }
+}
---
+++ src/SkillCraft.Api.Infrastructure/EventSerializer.cs
@@ -0,0 +1,18 @@
+﻿using SkillCraft.Api.Infrastructure.Converters;
+
+namespace SkillCraft.Api.Infrastructure;
+
+internal class EventSerializer : Logitar.EventSourcing.Infrastructure.EventSerializer
+{
+  protected override void RegisterConverters()
+  {
+    base.RegisterConverters();
+
+    SerializerOptions.Converters.Add(new CustomizationIdConverter());
+    SerializerOptions.Converters.Add(new DescriptionConverter());
+    SerializerOptions.Converters.Add(new NameConverter());
+    SerializerOptions.Converters.Add(new SummaryConverter());
+    SerializerOptions.Converters.Add(new UserIdConverter());
+    SerializerOptions.Converters.Add(new WorldIdConverter());
+  }
+}
