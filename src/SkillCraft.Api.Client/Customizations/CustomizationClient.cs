5c320ebcb88a8d7de73fd59553f761bc7a23d2d6
Francis Pion <francispion@hotmail.com>
2026-02-03 21:50:31 -0500

    client

---
+++ src/SkillCraft.Api.Client/Customizations/CustomizationClient.cs
@@ -0,0 +1,32 @@
+﻿using Krakenar.Contracts.Search;
+using SkillCraft.Api.Contracts.Customizations;
+
+namespace SkillCraft.Api.Client.Customizations;
+
+internal class CustomizationClient : ICustomizationService // TODO(fpion): CustomizationClient with RequestContext
+{
+  public async Task<CreateOrReplaceCustomizationResult> CreateOrReplaceAsync(CreateOrReplaceCustomizationPayload payload, Guid? id, CancellationToken cancellationToken)
+  {
+    return new CreateOrReplaceCustomizationResult(new CustomizationModel(), Created: false); // TODO(fpion): implement
+  }
+
+  public async Task<CustomizationModel?> DeleteAsync(Guid id, CancellationToken cancellationToken)
+  {
+    return null; // TODO(fpion): implement
+  }
+
+  public async Task<CustomizationModel?> ReadAsync(Guid id, CancellationToken cancellationToken)
+  {
+    return null; // TODO(fpion): implement
+  }
+
+  public async Task<SearchResults<CustomizationModel>> SearchAsync(SearchCustomizationsPayload payload, CancellationToken cancellationToken)
+  {
+    return new SearchResults<CustomizationModel>(); // TODO(fpion): implement
+  }
+
+  public async Task<CustomizationModel?> UpdateAsync(Guid id, UpdateCustomizationPayload payload, CancellationToken cancellationToken)
+  {
+    return null; // TODO(fpion): implement
+  }
+}
---
+++ src/SkillCraft.Api.Client/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SkillCraft.Api.Client.Customizations;
+using SkillCraft.Api.Contracts.Customizations;
+
+namespace SkillCraft.Api.Client;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddSkillCraftApiClient(this IServiceCollection services)
+  {
+    return services.AddTransient<ICustomizationService, CustomizationClient>();
+  }
+}
