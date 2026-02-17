81154c8231e8dff45886886f242e8611114914b1
Francis Pion <francispion@hotmail.com>
2026-02-17 14:06:38 -0500

    Completed Talents.

---
+++ src/SkillCraft.Api.Core/Talents/InvalidTalentRequirementException.cs
@@ -0,0 +1,75 @@
+﻿using Krakenar.Contracts;
+using Logitar;
+
+namespace SkillCraft.Api.Core.Talents;
+
+public class InvalidTalentRequirementException : DomainException
+{
+  private const string ErrorMessage = "The required talent tier should be lower than or equal to the requiring talent tier.";
+
+  public Guid WorldId
+  {
+    get => (Guid)Data[nameof(WorldId)]!;
+    private set => Data[nameof(WorldId)] = value;
+  }
+  public Guid RequiringTalentId
+  {
+    get => (Guid)Data[nameof(RequiringTalentId)]!;
+    private set => Data[nameof(RequiringTalentId)] = value;
+  }
+  public int RequiringTalentTier
+  {
+    get => (int)Data[nameof(RequiringTalentTier)]!;
+    private set => Data[nameof(RequiringTalentTier)] = value;
+  }
+  public Guid RequiredTalentId
+  {
+    get => (Guid)Data[nameof(RequiredTalentId)]!;
+    private set => Data[nameof(RequiredTalentId)] = value;
+  }
+  public int RequiredTalentTier
+  {
+    get => (int)Data[nameof(RequiredTalentTier)]!;
+    private set => Data[nameof(RequiredTalentTier)] = value;
+  }
+  public string PropertyName
+  {
+    get => (string)Data[nameof(PropertyName)]!;
+    private set => Data[nameof(PropertyName)] = value;
+  }
+
+  public override Error Error
+  {
+    get
+    {
+      Error error = new(this.GetErrorCode(), ErrorMessage);
+      error.Data[nameof(WorldId)] = WorldId;
+      error.Data[nameof(RequiringTalentId)] = RequiringTalentId;
+      error.Data[nameof(RequiringTalentTier)] = RequiringTalentTier;
+      error.Data[nameof(RequiredTalentId)] = RequiredTalentId;
+      error.Data[nameof(RequiredTalentTier)] = RequiredTalentTier;
+      error.Data[nameof(PropertyName)] = PropertyName;
+      return error;
+    }
+  }
+
+  public InvalidTalentRequirementException(Talent requiringTalent, Talent requiredTalent, string propertyName)
+    : base(BuildMessage(requiringTalent, requiredTalent, propertyName))
+  {
+    WorldId = requiringTalent.WorldId.ToGuid();
+    RequiringTalentId = requiringTalent.EntityId;
+    RequiringTalentTier = requiringTalent.Tier.Value;
+    RequiredTalentId = requiredTalent.EntityId;
+    RequiredTalentTier = requiredTalent.Tier.Value;
+    PropertyName = propertyName;
+  }
+
+  private static string BuildMessage(Talent requiringTalent, Talent requiredTalent, string propertyName) => new ErrorMessageBuilder(ErrorMessage)
+    .AddData(nameof(WorldId), requiringTalent.WorldId.ToGuid())
+    .AddData(nameof(RequiringTalentId), requiringTalent.EntityId)
+    .AddData(nameof(RequiringTalentTier), requiringTalent.Tier)
+    .AddData(nameof(RequiredTalentId), requiredTalent.EntityId)
+    .AddData(nameof(RequiredTalentTier), requiredTalent.Tier)
+    .AddData(nameof(PropertyName), propertyName)
+    .Build();
+}
---
+++ tests/SkillCraft.Api.UnitTests/Core/Talents/Commands/UpdateTalentCommandHandlerTests.cs
@@ -0,0 +1,74 @@
+﻿using Bogus;
+using Moq;
+using SkillCraft.Api.Contracts;
+using SkillCraft.Api.Contracts.Talents;
+using SkillCraft.Api.Core.Permissions;
+using SkillCraft.Api.Core.Storages;
+
+namespace SkillCraft.Api.Core.Talents.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class UpdateTalentCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+  private readonly Faker _faker = new();
+
+  private readonly UnitTestContext _context = UnitTestContext.Generate();
+  private readonly Mock<ITalentQuerier> _talentQuerier = new();
+  private readonly Mock<ITalentRepository> _talentRepository = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly UpdateTalentCommandHandler _handler;
+
+  public UpdateTalentCommandHandlerTests()
+  {
+    _handler = new UpdateTalentCommandHandler(
+      _context,
+      _permissionService.Object,
+      _talentQuerier.Object,
+      _talentRepository.Object,
+      _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should throw EntityNotFoundException when the required talent does not exist.")]
+  public async Task Given_NonExistentRequiredTalentId_When_HandleAsync_Then_EntityNotFoundException()
+  {
+    Talent talent = new(_context.World, new Tier(0), new Name("Melee"), _context.UserId);
+    _talentRepository.Setup(x => x.LoadAsync(talent.Id, _cancellationToken)).ReturnsAsync(talent);
+
+    _permissionService.Setup(x => x.CheckAsync(Actions.Update, talent, _cancellationToken)).Returns(Task.CompletedTask);
+
+    Guid requiredTalentId = Guid.Empty;
+    UpdateTalentPayload payload = new()
+    {
+      RequiredTalentId = new Update<Guid?>(requiredTalentId)
+    };
+
+    UpdateTalentCommand command = new(talent.EntityId, payload);
+    var exception = await Assert.ThrowsAsync<EntityNotFoundException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Equal(Talent.EntityKind, exception.EntityKind);
+    Assert.Equal(requiredTalentId, exception.EntityId);
+    Assert.Equal("RequiredTalentId", exception.PropertyName);
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    UpdateTalentPayload payload = new()
+    {
+      Name = _faker.Random.String(length: 999),
+      Summary = new Update<string>(_faker.Random.String(length: 999)),
+      Skill = new Update<GameSkill?>((GameSkill)(-1))
+    };
+
+    UpdateTalentCommand command = new(Guid.Empty, payload);
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Equal(3, exception.Errors.Count());
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "MaximumLengthValidator" && e.PropertyName == "Name");
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "MaximumLengthValidator" && e.PropertyName == "Summary.Value");
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "EnumValidator" && e.PropertyName == "Skill.Value");
+  }
+}
