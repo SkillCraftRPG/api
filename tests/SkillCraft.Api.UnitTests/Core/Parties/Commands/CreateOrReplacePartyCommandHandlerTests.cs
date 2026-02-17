bfed27e3a773f09b4c04793a4aa05373d49faf27
Francis Pion <francispion@hotmail.com>
2026-02-17 11:46:12 -0500

    Completed Worlds & Parties.

---
+++ tests/SkillCraft.Api.UnitTests/Core/Parties/Commands/CreateOrReplacePartyCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using SkillCraft.Api.Contracts.Parties;
+using SkillCraft.Api.Core.Permissions;
+using SkillCraft.Api.Core.Storages;
+
+namespace SkillCraft.Api.Core.Parties.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class CreateOrReplacePartyCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+
+  private readonly UnitTestContext _context = UnitTestContext.Generate();
+  private readonly Mock<IPartyQuerier> _partyQuerier = new();
+  private readonly Mock<IPartyRepository> _partyRepository = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly CreateOrReplacePartyCommandHandler _handler;
+
+  public CreateOrReplacePartyCommandHandlerTests()
+  {
+    _handler = new CreateOrReplacePartyCommandHandler(
+      _context,
+      _partyQuerier.Object,
+      _partyRepository.Object,
+      _permissionService.Object,
+      _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    CreateOrReplacePartyPayload payload = new()
+    {
+      Name = string.Empty
+    };
+
+    CreateOrReplacePartyCommand command = new(payload, Id: null);
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Single(exception.Errors);
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "NotEmptyValidator" && e.PropertyName == "Name");
+  }
+}
---
+++ tests/SkillCraft.Api.UnitTests/Core/Parties/Commands/UpdatePartyCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using Moq;
+using SkillCraft.Api.Contracts.Parties;
+using SkillCraft.Api.Core.Permissions;
+using SkillCraft.Api.Core.Storages;
+
+namespace SkillCraft.Api.Core.Parties.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class UpdatePartyCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+  private readonly Faker _faker = new();
+
+  private readonly UnitTestContext _context = UnitTestContext.Generate();
+  private readonly Mock<IPartyQuerier> _partyQuerier = new();
+  private readonly Mock<IPartyRepository> _partyRepository = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly UpdatePartyCommandHandler _handler;
+
+  public UpdatePartyCommandHandlerTests()
+  {
+    _handler = new UpdatePartyCommandHandler(
+      _context,
+      _partyQuerier.Object,
+      _partyRepository.Object,
+      _permissionService.Object,
+      _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    UpdatePartyPayload payload = new()
+    {
+      Name = _faker.Random.String(length: 999)
+    };
+
+    UpdatePartyCommand command = new(Guid.Empty, payload);
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Single(exception.Errors);
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "MaximumLengthValidator" && e.PropertyName == "Name");
+  }
+}
