af7acb278151c015dc4d5522d1df7a2c1e1d4bd1
Francis Pion <francispion@hotmail.com>
2026-02-17 11:47:59 -0500

    Completed Worlds & Parties. (#15)

---
+++ tests/SkillCraft.Api.UnitTests/Core/Worlds/Commands/CreateOrReplaceWorldCommandHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using SkillCraft.Api.Contracts.Worlds;
+using SkillCraft.Api.Core.Permissions;
+using SkillCraft.Api.Core.Storages;
+
+namespace SkillCraft.Api.Core.Worlds.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class CreateOrReplaceWorldCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+
+  private readonly UnitTestContext _context = UnitTestContext.Generate();
+  private readonly Mock<IWorldQuerier> _worldQuerier = new();
+  private readonly Mock<IWorldRepository> _worldRepository = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly CreateOrReplaceWorldCommandHandler _handler;
+
+  public CreateOrReplaceWorldCommandHandlerTests()
+  {
+    _handler = new CreateOrReplaceWorldCommandHandler(
+      _context,
+      _worldQuerier.Object,
+      _worldRepository.Object,
+      _permissionService.Object,
+      _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    CreateOrReplaceWorldPayload payload = new()
+    {
+      Name = string.Empty
+    };
+
+    CreateOrReplaceWorldCommand command = new(payload, Id: null);
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Single(exception.Errors);
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "NotEmptyValidator" && e.PropertyName == "Name");
+  }
+}
---
+++ tests/SkillCraft.Api.UnitTests/Core/Worlds/Commands/UpdateWorldCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using Bogus;
+using Moq;
+using SkillCraft.Api.Contracts.Worlds;
+using SkillCraft.Api.Core.Permissions;
+using SkillCraft.Api.Core.Storages;
+
+namespace SkillCraft.Api.Core.Worlds.Commands;
+
+[Trait(Traits.Category, Categories.Unit)]
+public class UpdateWorldCommandHandlerTests
+{
+  private readonly CancellationToken _cancellationToken = default;
+  private readonly Faker _faker = new();
+
+  private readonly UnitTestContext _context = UnitTestContext.Generate();
+  private readonly Mock<IWorldQuerier> _worldQuerier = new();
+  private readonly Mock<IWorldRepository> _worldRepository = new();
+  private readonly Mock<IPermissionService> _permissionService = new();
+  private readonly Mock<IStorageService> _storageService = new();
+
+  private readonly UpdateWorldCommandHandler _handler;
+
+  public UpdateWorldCommandHandlerTests()
+  {
+    _handler = new UpdateWorldCommandHandler(
+      _context,
+      _worldQuerier.Object,
+      _worldRepository.Object,
+      _permissionService.Object,
+      _storageService.Object);
+  }
+
+  [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
+  public async Task Given_InvalidPayload_When_HandleAsync_Then_ValidationException()
+  {
+    UpdateWorldPayload payload = new()
+    {
+      Name = _faker.Random.String(length: 999)
+    };
+
+    UpdateWorldCommand command = new(Guid.Empty, payload);
+    var exception = await Assert.ThrowsAsync<FluentValidation.ValidationException>(async () => await _handler.HandleAsync(command, _cancellationToken));
+
+    Assert.Single(exception.Errors);
+    Assert.Contains(exception.Errors, e => e.ErrorCode == "MaximumLengthValidator" && e.PropertyName == "Name");
+  }
+}
