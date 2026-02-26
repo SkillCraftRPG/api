a0ab5f8a696d64719b8864370956df78229a6fe9
Francis Pion <francispion@hotmail.com>
2026-02-26 13:53:16 -0500

    todos

--- src/SkillCraft.Api.Contracts/Characters/CreateCharacterPayload.cs
+++ src/SkillCraft.Api.Contracts/Characters/CreateCharacterPayload.cs
@@ -17,9 +17,6 @@ public record CreateCharacterPayload
 
   // TODO(fpion): Skill Ranks
   // TODO(fpion): Starting Wealth & Inventory (Attacks, Weapons, Shields, Armor, etc.)
-  // TODO(fpion): Bonuses
-  // TODO(fpion): Picture URL
-  // TODO(fpion): Token URL
 
   public CreateCharacterPayload() : this(string.Empty)
   {
