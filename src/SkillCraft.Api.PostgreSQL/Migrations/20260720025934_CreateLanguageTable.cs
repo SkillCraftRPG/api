3a52262260c9daf720282ca4e52c664402892bca
Francis Pion <francispion@hotmail.com>
2026-07-19 23:00:24 -0400

    migration

---
+++ src/SkillCraft.Api.PostgreSQL/Migrations/20260720025934_CreateLanguageTable.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SkillCraft.Api.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateLanguageTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                schema: "Game",
+                columns: table => new
+                {
+                    LanguageId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    WorldId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    ScriptId = table.Column<int>(type: "integer", nullable: true),
+                    TypicalSpeakers = table.Column<string>(type: "text", nullable: true),
+                    Version = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.LanguageId);
+                    table.ForeignKey(
+                        name: "FK_Languages_Scripts_ScriptId",
+                        column: x => x.ScriptId,
+                        principalSchema: "Game",
+                        principalTable: "Scripts",
+                        principalColumn: "ScriptId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Languages_Worlds_WorldId",
+                        column: x => x.WorldId,
+                        principalSchema: "Game",
+                        principalTable: "Worlds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_ScriptId",
+                schema: "Game",
+                table: "Languages",
+                column: "ScriptId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_CreatedBy",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "CreatedBy" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_CreatedOn",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "CreatedOn" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_Id",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "Id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_Name",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "Name" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_ScriptId",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "ScriptId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_UpdatedBy",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "UpdatedBy" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_UpdatedOn",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "UpdatedOn" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Languages_WorldId_Version",
+                schema: "Game",
+                table: "Languages",
+                columns: new[] { "WorldId", "Version" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Languages",
+                schema: "Game");
+        }
+    }
+}
