9f49c32bb661901351d3d04f34a5d0d3cf961f4b
Francis Pion <francispion@hotmail.com>
2026-08-14 01:01:39 -0400

    update

---
+++ src/SkillCraft.Api.Core/Characters/Commands/UpdateCharacter.cs
@@ -0,0 +1,45 @@
+﻿using Logitar.CQRS;
+using SkillCraft.Api.Core.Characters.Models;
+using SkillCraft.Api.Core.Permissions;
+
+namespace SkillCraft.Api.Core.Characters.Commands;
+
+internal record UpdateCharacterCommand(Guid Id, UpdateCharacterPayload Payload) : ICommand<CharacterModel?>;
+
+internal class UpdateCharacterCommandHandler : ICommandHandler<UpdateCharacterCommand, CharacterModel?>
+{
+  private readonly ICharacterQuerier _characterQuerier;
+  private readonly ICharacterRepository _characterRepository;
+  private readonly IContext _context;
+  private readonly IPermissionService _permissionService;
+
+  public UpdateCharacterCommandHandler(
+    ICharacterQuerier characterQuerier,
+    ICharacterRepository characterRepository,
+    IContext context,
+    IPermissionService permissionService)
+  {
+    _characterQuerier = characterQuerier;
+    _characterRepository = characterRepository;
+    _context = context;
+    _permissionService = permissionService;
+  }
+
+  public async Task<CharacterModel?> HandleAsync(UpdateCharacterCommand command, CancellationToken cancellationToken)
+  {
+    UpdateCharacterPayload payload = command.Payload;
+    payload.Validate();
+
+    CharacterId characterId = new(_context.WorldId, command.Id);
+    Character? character = await _characterRepository.LoadAsync(characterId, cancellationToken);
+    if (character is null)
+    {
+      return null;
+    }
+    await _permissionService.CheckAsync(Actions.Update, character, cancellationToken);
+
+    // TODO(fpion): implement
+
+    return await _characterQuerier.ReadAsync(character, cancellationToken);
+  }
+}
---
+++ src/SkillCraft.Api.Core/Characters/Models/UpdateCharacterPayload.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using SkillCraft.Api.Core.Validation;
+
+namespace SkillCraft.Api.Core.Characters.Models;
+
+public record UpdateCharacterPayload
+{
+  public string? Name { get; set; }
+  public Optional<DominantHand?>? DominantHand { get; set; }
+
+  public void Validate() => new Validator().ValidateAndThrow(this);
+
+  private class Validator : AbstractValidator<UpdateCharacterPayload>
+  {
+    public Validator()
+    {
+      When(x => !string.IsNullOrWhiteSpace(x.Name), () => RuleFor(x => x.Name!).Name());
+      When(x => x.DominantHand?.Value is not null, () => RuleFor(x => x.DominantHand!.Value!.Value).IsInEnum());
+    }
+  }
+}
